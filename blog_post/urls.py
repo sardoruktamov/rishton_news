6b6adff9fb5d53bd35e9eefa173402f1ddfd0e36
sardoruktamov <sardorbek.uktamov.1@mail.ru>
2021-09-06 21:56:43 +0500

    tags filter create

--- blog_post/urls.py
+++ blog_post/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import HomePageView, DetailViews, BlogPageView, CategoriesView
+from .views import HomePageView, DetailViews, BlogPageView, CategoriesView, tagsfilter
 
 urlpatterns = [
     path('', HomePageView.as_view(), name="homepage"),
     path('blogs/', BlogPageView.as_view(), name="blogpage"),
+    path('tagged/<int:id>', tagsfilter, name="tagged_blogs"),
     path('blog/<str:slug>/', DetailViews.as_view(), name='detailview'),
     path('categories/<str:slug>/', CategoriesView.as_view(), name='categories')
 ]
--- blog_post/views.py
+++ blog_post/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.views.generic import ListView, DetailView
-from .models import Category, Blog, PicturesFromTheBlog
+from .models import Category, Blog, PicturesFromTheBlog, Tags
+
 
 # Create your views here.
 class HomePageView(ListView):
@@ -9,7 +10,7 @@ class HomePageView(ListView):
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data()
-        context['blog'] = Blog.objects.all()
+        context['tags'] = Tags.objects.all()
         return context
 
 
@@ -25,6 +26,7 @@ class DetailViews(DetailView):
         context = super(DetailViews, self).get_context_data(**kwargs)
         return context
 
+
 class BlogPageView(ListView):
     template_name = 'blog.html'
     queryset = Blog.objects.all()
@@ -33,8 +35,9 @@ class BlogPageView(ListView):
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data()
-        # context['blogs'] = Blog.objects.all()
+        context['blogs'] = Blog.objects.all()
         return context
+
 
 class CategoriesView(ListView):
     template_name = 'blog.html'
@@ -43,3 +46,26 @@ class CategoriesView(ListView):
     def get_queryset(self):
         queryset = self.model.objects.filter(category__slug=self.kwargs.get('slug'))
         return queryset
+
+
+def tagsfilter(request, id):
+    tags = get_object_or_404(Tags, pk=id)
+    all_tags = Tags.objects.all()
+    nat = Blog.objects.filter(tags=tags)
+    context = {
+        "object_list": nat,
+        "tags": all_tags
+    }
+    return render(request, "blog.html", context)
+
+# class TagListView(ListView):
+#     """bloglarni taglar yordamida filterlash"""
+#     template_name = "blog.html"
+#
+#     def get_queryset(self, id):
+#         return Blog.objects.filter(id__slug=self.kwargs.get("id")).all()
+#
+#     # def get_context_data(self, **kwargs):
+#     #     context = super(TagListView, self).get_context_data(**kwargs)
+#     #     context["tags"] = self.kwargs.get("name")
+#     #     return context
