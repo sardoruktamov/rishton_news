3832370412aa550ebfad645c8364f317dc773db8
sardoruktamov <sardorbek.uktamov.1@mail.ru>
2021-09-10 21:48:59 +0500

    elon app create

---
+++ elon/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class ElonConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'elon'
--- rishton_news/settings.py
+++ rishton_news/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     #my apps
     'blog_post',
+    'elon',
 
     #other apps
     'parler',
