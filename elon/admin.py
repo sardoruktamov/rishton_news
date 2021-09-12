375172d3fb1596a2dfc65f54e2b62432f98cba60
sardoruktamov <sardorbek.uktamov.1@mail.ru>
2021-09-12 14:50:15 +0500

    elon model add in admin

--- elon/admin.py
+++ elon/admin.py
@@ -1,3 +1,47 @@
 from django.contrib import admin
+from .models import Category, Subcategory, Announcement, PicturesFromTheAnnouncement
+from parler.admin import TranslatableAdmin
+
 
 # Register your models here.
+
+@admin.register(Category)
+class CategoryAdmin(TranslatableAdmin):
+    list_display = ('name',)
+
+    def get_prepopulated_fields(self, request, obj=None):
+        return {'slug': ('name',)}
+
+@admin.register(Subcategory)
+class CategoryAdmin(TranslatableAdmin):
+    list_display = ('name', 'category',)
+
+    def get_prepopulated_fields(self, request, obj=None):
+        return {'slug': ('name',)}
+
+class AnnouncementImageAdmin(admin.StackedInline):
+    model = PicturesFromTheAnnouncement
+
+@admin.register(Announcement)
+class BlogAdmin(TranslatableAdmin):
+    list_display = ('title', 'description', 'category',)
+    list_display_links = ('title',)
+    search_fields = ['title', 'description', 'category']
+    inlines = [AnnouncementImageAdmin]
+    list_filter = ('category', 'created_at')
+    save_on_top = True  # saqlash va o`chirish tugmalarini sahifa yuqorisiga ham qo`shimcha sifatida olib chiqish uchun
+    save_as = True
+
+    class Meta:
+        model = Announcement
+
+    def get_prepopulated_fields(self, request, obj=None):
+        return {'slug': ('title',)}
+
+
+@admin.register(PicturesFromTheAnnouncement)
+class AnnouncementPicturesAdmin(admin.ModelAdmin):  # PicturesAdmin(TranslatableAdmin) bunday qilib yozib bo`lmaydi xatolik beradi
+    list_display = ('owner',)
+
+
+
