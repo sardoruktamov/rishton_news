4048e587f2fc18eecfd3284196407022abccb71a
sardoruktamov <sardorbek.uktamov.1@mail.ru>
2021-09-12 17:02:54 +0500

    list and create announcements add

--- elon/views.py
+++ elon/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from django.views.generic import ListView, CreateView
+from .models import Category, Subcategory, Announcement
+from django.urls import reverse_lazy
+from .forms import AnnouncementForm
 
-# Create your views here.
+class AnnouncementList(ListView):
+    model = Announcement
+    context_object_name = "announcement"
+    template_name = "announcement/announcements.html"
+
+class AnnouncementCreateView(CreateView):
+    model = Announcement
+    form_class = AnnouncementForm
+    template_name = "announcement/add.html"
+    success_url = reverse_lazy('announcement_list')
