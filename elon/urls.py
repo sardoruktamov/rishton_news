de58c0ec6c1e6dc4544722c91d3ac53061314272
sardoruktamov <sardorbek.uktamov.1@mail.ru>
2021-09-14 20:22:11 +0500

    drop down add

--- elon/urls.py
+++ elon/urls.py
@@ -1,5 +1,9 @@
 from django.urls import path
+from .views import AnnouncementList, AnnouncementCreateView, load_category
+
 
 urlpatterns = [
-
+    path(" ", AnnouncementList.as_view(), name="announcement_list"),
+    path('create/', AnnouncementCreateView.as_view(), name="announcement_create"),
+    path('ajax/load-subcategory/', load_category, name="ajax_load_subcategory"),
 ]
--- elon/views.py
+++ elon/views.py
@@ -14,3 +14,9 @@ class AnnouncementCreateView(CreateView):
     form_class = AnnouncementForm
     template_name = "announcement/add.html"
     success_url = reverse_lazy('announcement_list')
+
+def load_category(request):
+    category_id = request.GET.get('category')
+    subcategory = Subcategory.objects.filter(category_id=category_id).order_by('name')
+    # print(subcategory,'--------------', category_id,'8888888888888888')
+    return render(request, "announcement/category_dropdown.html", {'subcategory': subcategory})
