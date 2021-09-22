c29b272cfe36bedd857e03d0dba15e0e90adfc36
sardoruktamov <sardorbek.uktamov.1@mail.ru>
2021-09-22 20:24:48 +0500

    update create

--- elon/views.py
+++ elon/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.views.generic import ListView, CreateView
+from django.views.generic import ListView, CreateView, UpdateView
 from .models import Category, Subcategory, Announcement
 from django.urls import reverse_lazy
 from .forms import AnnouncementForm
@@ -19,3 +19,9 @@ def load_category(request):
     category_id = request.GET.get('category')
     subcategory = Subcategory.objects.filter(category_id=category_id).order_by('name')
     return render(request, "announcement/category_dropdown.html", {'subcategory': subcategory})
+
+class AnnouncementUpdateView(UpdateView):
+    model = Announcement
+    form_class = AnnouncementForm
+    # template_name = "announcement/add.html"
+    success_url = reverse_lazy('announcement_list')
