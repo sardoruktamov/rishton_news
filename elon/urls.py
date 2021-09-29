a80769367f0f7cee8227f491f7e12f70d428bff9
sardoruktamov <sardorbek.uktamov.1@mail.ru>
2021-09-29 21:46:19 +0500

    change

--- elon/urls.py
+++ elon/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from .views import AnnouncementList, AnnouncementCreateView, load_category, AnnouncementDetailView, add
 
 urlpatterns = [
-    path(" ", AnnouncementList.as_view(), name="announcement_list"),
+    path("", AnnouncementList.as_view(), name="announcement_list"),
     path('create/', AnnouncementCreateView.as_view(), name="announcement_create"),
     path('Announcement/<slug:slug>', AnnouncementDetailView.as_view(), name="ann_detail"),
     path('add/', add, name="announcement_add"),
